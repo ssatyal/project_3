a6199fadf09adc73c3f67fe448ff5481733f9acd
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-04-04 17:03:09 -0400

    angular routes showing up

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -10,17 +10,47 @@
     "$stateProvider",
     RouterFunction
   ])
+  .factory( "TripFactory", [
+        "$resource",
+        TripFactory
+      ])
+  .controller("indexCtrl", [
+    "TripFactory",
+    indexControllerFunction
+  ])
+  .controller( "showCtrl", [
+    "TripFactory",
+    "$stateParams",
+    showStrlFunction
+  ]);
+
+  function showStrlFunction( TripFactory, $stateParams ){
+  this.trip = TripFactory.get({id: $stateParams.id});
+  };
+
+  function indexControllerFunction(Trip){
+    var indexVM = this;
+    indexVM.trips = Trip.all;
+  };
+
+  function TripFactory( $resource ){
+    var Trip = $resource( "http://localhost:3000/trips/:id.json", {}, {
+      update: { method: "PUT" }
+    });
+    Trip.all = Trip.query();
+    return Trip;
+  };
 
   function RouterFunction($stateProvider){
     $stateProvider
-    .state("index", {
-      url: "/",
-      templateUrl: "ng-views/welcome.html",
+    .state("tripIndex", {
+      url: "/trips",
+      templateUrl: "ng-views/trip.index.html",
       controller: "indexCtrl",
-      controllerAs: "indexViewModel"
+      controllerAs: "indexVM"
     })
     .state("show", {
-      url: "/:id",
+      url: "/trips/:id",
       templateUrl: "ng-views/trip.show.html",
       controller: "showCtrl",
       controllerAs: "showVM"
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,15 +1,17 @@
 class TripsController < ApplicationController
-
-  def home
-    response =
-  HTTParty.get("http://rubygems.org/api/v1/versions/httparty.json")
-    # @client = GooglePlaces::Client.new(AIzaSyDRtvnExjmDi4aj1v7eUJuOP2dH74x1Xww)
-    # @client.spots(-33.8670522,151.1957362)
-    # @places = Places.all
-    # respond_to do |format|
-    #   format.html
-    #   format.json {render json: @places, status: :ok}
+  before_action do
+    if params[:id]
+      @trip = Trip.find(params[:id])
     end
-
-
   end
+  def index
+    @trips = Trip.all
+    respond_to do |format|
+    format.html
+    format.json{ render json: @trips, status: :ok}
+    end
+  end
+  def show
+    render json: @trip, status: :ok
+  end
+end
