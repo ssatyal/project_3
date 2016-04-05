dee723f53943ba00f1a2e6463cce85ab94d6175b
Keith Prifte <kprift@gmail.com>
2016-04-05 14:25:52 -0400

    adds verify false for HTTParty get method

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,14 +1,13 @@
-
 class Place
 
   def read
       # google API which returns places within the specified radius of the given location coordinates
     url = "https://maps.googleapis.com/maps/api/place/radarsearch/json?location=51.503186,-0.126446&radius=500&type=museum&key=#{ENV["google_api_key"]}"
 
-    # HTTParty calls the google API and we are storing the API response a varialbe called response
-    response = HTTParty.get(url)
+    # HTTParty calls the google API and we are storing the API response in a varialbe called response
+    response = HTTParty.get(url, :verify => false)
 
-    # the bulk of the data we need is in the results portion of the response object
+    # the bulk of the data we need is in the "results" object of the response, which we are isolating here
     responses = response["results"]
 
     # we are looping through the responses, parsing out the place_id, and storing all of the id's in an instance variable
@@ -23,7 +22,7 @@ class Place
     @place_name = []
     @place_ids.each do |id|
       url  = "https://maps.googleapis.com/maps/api/place/details/json?placeid=#{id}&key=#{ENV["google_api_key"]}"
-      response = HTTParty.get(url)
+      response = HTTParty.get(url, :verify => false)
      @place_name << response["result"]["name"]
     end
     @place_name
